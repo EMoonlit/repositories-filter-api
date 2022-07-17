94a3b14519b96bb0ee91a85d844be2d9a1da0241
Emerson Junqueira <emoonlit@gmail.com>
2022-07-17 17:42:21 -0300

    refactor: add folder to interfaces and updated errorHandler, add cache

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,45 @@
 import express from 'express';
+import 'express-async-errors';
 import 'dotenv/config';
 import { errorHandler } from './middlewares/errorHandler';
-import { Repository } from './services/Repositories.service';
+import { RequestRepositories } from './services/Repositories.service';
+import NodeCache from 'node-cache';
 
 const app = express();
+const cache = new NodeCache({ stdTTL: 600 })
+
 app.use(express.json());
 
-app.get('/', async (req, res) => {
+app.get('/', (_req, res) => {
+  res.status(200).json({
+    status: 200,
+    message: 'Ok'
+  })
+})
+
+app.get('/find', async (req, res) => {
   const { organization, lang } = req.query;
-  const repository = new Repository()
-  const repos = await repository.requestRepositories(`https://api.github.com/orgs/${organization}/repos`, lang as string);
-  res.status(200).json(repos);
-});
+
+  const url = `https://api.github.com/orgs/${organization}/repos`;
+
+  let repositoriesInCache = cache.get(`${organization}-${lang}`);
+
+
+  if (repositoriesInCache === undefined) {
+    const repository = new RequestRepositories()
+    const repos = await repository.requestRepositories(url, lang as string);
+    cache.set(`${organization}-${lang}`, repos);
+    repositoriesInCache = repos;
+  }
+  res.status(200).json(repositoriesInCache)
+}
+)
+app.use('*', (req, res) => {
+  res.status(404).json({
+    status: 404,
+    message: 'Not Found'
+  })
+})
 
 app.use(errorHandler);
 
--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,6 +1,13 @@
+import { AxiosError } from "axios";
 import { Response, Request, NextFunction } from "express";
 
 export const errorHandler = (err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  if (err instanceof AxiosError) {
+    return res.status(404).json({
+      status: 404,
+      message: err.message
+    })
+  }
   res.status(500).json({
     status: 500,
     message: err.message
